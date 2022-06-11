f85874a0646b8468919f9e40cf294c9d37740a8b
Jason <jasoncmcvicar@gmail.com>
2022-06-10 19:32:01 -0500

    working through matrix in javascript

--- static/boggle.js
+++ static/boggle.js
@@ -22,15 +22,18 @@ async function start() {
 /** Display board */
 
 function displayBoard(board) {
-  // $table.empty();
+  $table.empty();
   // loop over board and create the DOM tr/td structure
-  $tableRow = $("tr");
-  $tableData = $("td");
+  let $tableRow = $('<tr>');
+  let $tableData = $('<td>');
+  let $tableBody = $('<tbody>');
+  $('<div></div>')
+  $table.append($tableBody);
 
   for (let i = 0; i < board.length; i++) {
-    $table.append($tableRow).attr("id", i)
+    $tableBody.append($tableRow.attr("id", i));
     for (let j = 0; j < board[i].length; j++) {
-
+      $(`#${i}`).append($tableData.text(board[i][j]));
     }
   }
 }
